529f4f52d0b9f4fb494bb481b9cb8c151a14d12b
David Bai <baiminghan@gmail.com>
2022-09-13 10:40:04 +1200

    setup the function to listening to real time data change on firebase database

--- graph.js
+++ graph.js
@@ -14,15 +14,49 @@ const svg = d3.select('.canvas')
     .append('svg')
     .attr('width', dims.width + 150)
     .attr('height', dims.height + 150)
-
 const graph = svg.append('g')
     .attr('transform', `translate(${cent.x}, ${cent.y})`);
 
-
+// pic chart config
 const pie = d3.pie()
     .sort(null)
     .value(d => d.cost);
 
+
+
+// arc path config
 const arcPath = d3.arc()
     .outerRadius(dims.radius)
     .innerRadius(dims.radius / 2);
+
+// get data from firebase
+
+const data = [];
+
+db.collection('expenses').onSnapshot(res => {
+
+
+    res.docChanges().forEach(change => {
+
+        const doc = {
+            ...change.doc.data(),
+            id: change.doc.id
+        }
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+            case "modified":
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id != doc.id);
+                break;
+            default:
+                break;
+        }
+
+    });
+})
